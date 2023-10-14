2084fe508d06d0ca87ab3e0529527d4a5bdf82b1
Vysssaa <vysssaa@gmail.com>
2023-10-14 08:43:26 +0530

    src/Components/AgeCalc.js 	src/Components/DD.css 	src/Components/DummyData.js 	src/Components/Link.js
    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import WordCounter from './Components/WordCount';
 import Calculator from './Components/Calculator';
 import CP from './Components/ColorPickerExt';
+import AgeCalculator from './Components/AgeCalc';
+import Nav from './Components/Link';
+import DummyData from './Components/DummyData';
+import {BrowserRouter as HashRouter,Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div>
-      {/* <WordCounter /> */}
-      {/* <Calculator /> */}
-      {/* <CP /> */}
-      
+      <Routes>
+          <Route path="/Task1" element={<WordCounter />} />
+          <Route path="/Task2" element={<Calculator />} />
+          <Route path="/Task4" element={<DummyData />} />
+          <Route path="/Task5" element={<AgeCalculator />} />
+          <Route path="/" element={<Nav />} />
+      </Routes>
     </div>
   );
 }
