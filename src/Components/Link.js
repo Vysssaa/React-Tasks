d3c1e60570a8107cf2799117b20e7c5f2c4457ab
Vysssaa <143383470+Vysssaa@users.noreply.github.com>
2023-10-14 08:56:19 +0530

    Add files via upload

--- src/Components/Link.js
+++ src/Components/Link.js
@@ -8,18 +8,22 @@ function Nav()
         <table style={{border:"solid black 2px"}} cellSpacing={"5px"}>
             <tr>
                 <td>Task 1</td>
+                <td><a href="https://github.com/Vysssaa/React-Tasks">Repository Link</a></td>
                 <td><Link to="/Task1">Link 1</Link></td>
             </tr>
             <tr>
                 <td>Task 2</td>
+                <td><a href="https://github.com/Vysssaa/React-Tasks">Repository Link</a></td>
                 <td><Link to="/Task2">Link 2</Link></td>
             </tr>
             <tr>
                 <td>Task 4</td>
+                <td><a href="https://github.com/Vysssaa/React-Tasks">Repository Link</a></td>
                 <td><Link to="/Task4">Link 4</Link></td>
             </tr>
             <tr>
                 <td>Task 5</td>
+                <td><a href="https://github.com/Vysssaa/React-Tasks">Repository Link</a></td>
                 <td><Link to="/Task5">Link 5</Link></td>
             </tr>
             <tr>
